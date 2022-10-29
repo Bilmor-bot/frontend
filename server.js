ff1e2206f78b84945a8bdecb447bd55c5b0fd85e
roman.vorontsov <cflatminor92@gmail.com>
2022-10-29 13:30:59 +0300

    test ssl

--- server.js
+++ server.js
@@ -2,7 +2,11 @@
 
 const express = require("express");
 const app = express();
+const enforce = require('express-sslify');
 const port = process.env.PORT || 8000;
+
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(enforce.HTTPS({ trustXForwardedHostHeader: true }));
 
 app.get("/", (req, res) => {
     res.send("hello\n");
